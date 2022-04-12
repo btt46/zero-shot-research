eb5be4ef7f94f7e030b4b2f6e6e8307fbe54b23f
btt46 <tuanthanhbui46@gmail.com>
2022-04-12 23:45:00 +0900

    update

--- eval_ja2vi.sh
+++ eval_ja2vi.sh
@@ -18,35 +18,35 @@ TAGGED_DATA=$EXPDIR/data/evaluation/tmp/tagged-data
 CUDA_VISIBLE_DEVICES=$GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA \
             --input $TAGGED_DATA/valid.${src} \
             --path $MODEL \
-            --beam 5 | tee ${PWD}/results/${src}2${tgt}/valid_trans_result.${tgt}
+            --beam 5 | tee ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_trans_result.${tgt}
 
-grep ^H ${PWD}/results/${src}2${tgt}/valid_trans_result.${tgt} | cut -f3 > ${PWD}/results/${src}2${tgt}/valid_trans.${tgt}
-cat ${PWD}/results/${src}2${tgt}/valid_trans.${tgt} | sed -r 's/(@@ )|(@@ ?$)//g' > ${PWD}/results/${src}2${tgt}/valid_rmvbpe.${tgt}
+grep ^H ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_trans_result.${tgt} | cut -f3 > ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_trans.${tgt}
+cat ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_trans.${tgt} | sed -r 's/(@@ )|(@@ ?$)//g' > ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_rmvbpe.${tgt}
 
 # detruecase
-$DETRUECASER < ${PWD}/results/${src}2${tgt}/valid_rmvbpe.${tgt} > ${PWD}/results/${src}2${tgt}/valid_detruecase.${tgt}
+$DETRUECASER < ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_rmvbpe.${tgt} > ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_detruecase.${tgt}
 
 # detokenize
-python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${src}2${tgt}/valid_detruecase.${tgt} ${PWD}/results/${src}2${tgt}/valid.${tgt}
-python3.6 $PWD/preprocess/normalize.py ${PWD}/results/${src}2${tgt}/valid.${tgt} ${PWD}/results/${src}2${tgt}/valid_normalize.${tgt}
-echo "VALID" >> ${PWD}/results/${src}2${tgt}/valid_result.txt
-perl $PWD/multi-bleu.pl $PWD/data/evaluation/tmp/normalized/valid.${tgt} < ${PWD}/results/${src}2${tgt}/valid_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
+python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_detruecase.${tgt} ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid.${tgt}
+python3.6 $PWD/preprocess/normalize.py ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid.${tgt} ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_normalize.${tgt}
+echo "VALID" >> ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_result.txt
+perl $PWD/multi-bleu.pl $PWD/data/evaluation/tmp/normalized/valid.${tgt} < ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/valid_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/${MODEL_NAME}/valid_result.txt
 
 ########################## Test dataset #########################################
 
 CUDA_VISIBLE_DEVICES=$GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA \
             --input $TAGGED_DATA/test.${src} \
             --path $MODEL \
-            --beam 5 | tee ${PWD}/results/${src}2${tgt}/test_trans_result.${tgt}
+            --beam 5 | tee ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_trans_result.${tgt}
 
-grep ^H ${PWD}/results/${src}2${tgt}/test_trans_result.${tgt} | cut -f3 > ${PWD}/results/${src}2${tgt}/test_trans.${tgt}
-cat ${PWD}/results/${src}2${tgt}/test_trans.${tgt} | sed -r 's/(@@ )|(@@ ?$)//g' > ${PWD}/results/${src}2${tgt}/test_rmvbpe.${tgt}
+grep ^H ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_trans_result.${tgt} | cut -f3 > ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_trans.${tgt}
+cat ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_trans.${tgt} | sed -r 's/(@@ )|(@@ ?$)//g' > ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_rmvbpe.${tgt}
 
 # detruecase
-$DETRUECASER < ${PWD}/results/${src}2${tgt}/test_rmvbpe.${tgt} > ${PWD}/results/${src}2${tgt}/test_detruecase.${tgt}
+$DETRUECASER < ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_rmvbpe.${tgt} > ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_detruecase.${tgt}
 
 # detokenize
-python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${src}2${tgt}/test_detruecase.${tgt} ${PWD}/results/${src}2${tgt}/test.${tgt}
-python3.6 $PWD/preprocess/normalize.py ${PWD}/results/${src}2${tgt}/test.${tgt} ${PWD}/results/${src}2${tgt}/test_normalize.${tgt}
-echo "test" >> ${PWD}/results/${src}2${tgt}/test_result.txt
-perl $PWD/multi-bleu.pl $PWD/data/evaluation/tmp/normalized/test.${tgt} < ${PWD}/results/${src}2${tgt}/test_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
+python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_detruecase.${tgt} ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test.${tgt}
+python3.6 $PWD/preprocess/normalize.py ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test.${tgt} ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_normalize.${tgt}
+echo "test" >> ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_result.txt
+perl $PWD/multi-bleu.pl $PWD/data/evaluation/tmp/normalized/test.${tgt} < ${PWD}/results/${MODEL_NAME}/${src}2${tgt}/test_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/${MODEL_NAME}/valid_result.txt
