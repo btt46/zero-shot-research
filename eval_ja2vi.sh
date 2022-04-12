c70bd0199582e2be24b04a34c791c70f5c10820f
btt46 <tuanthanhbui46@gmail.com>
2022-04-12 16:34:36 +0900

    update

--- eval_ja2vi.sh
+++ eval_ja2vi.sh
@@ -30,7 +30,7 @@ $DETRUECASER < ${PWD}/results/${src}2${tgt}/valid_rmvbpe.${tgt} > ${PWD}/results
 python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${src}2${tgt}/valid_detruecase.${tgt} ${PWD}/results/${src}2${tgt}/valid.${tgt}
 python3.6 $PWD/preprocess/normalize.py ${PWD}/results/${src}2${tgt}/valid.${tgt} ${PWD}/results/${src}2${tgt}/valid_normalize.${tgt}
 echo "VALID" >> ${PWD}/results/${src}2${tgt}/valid_result.txt
-perl $PWD/multi-bleu.pl $PWD/data/tmp/normalized/valid.${tgt} < ${PWD}/results/${src}2${tgt}/valid_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
+perl $PWD/multi-bleu.pl $PWD/data/evaluation/tmp/normalized/valid.${tgt} < ${PWD}/results/${src}2${tgt}/valid_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
 
 ########################## Test dataset #########################################
 
@@ -49,4 +49,4 @@ $DETRUECASER < ${PWD}/results/${src}2${tgt}/test_rmvbpe.${tgt} > ${PWD}/results/
 python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${src}2${tgt}/test_detruecase.${tgt} ${PWD}/results/${src}2${tgt}/test.${tgt}
 python3.6 $PWD/preprocess/normalize.py ${PWD}/results/${src}2${tgt}/test.${tgt} ${PWD}/results/${src}2${tgt}/test_normalize.${tgt}
 echo "test" >> ${PWD}/results/${src}2${tgt}/test_result.txt
-perl $PWD/multi-bleu.pl $PWD/data/tmp/normalized/test.${tgt} < ${PWD}/results/${src}2${tgt}/test_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
+perl $PWD/multi-bleu.pl $PWD/data/evaluation/tmp/normalized/test.${tgt} < ${PWD}/results/${src}2${tgt}/test_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
