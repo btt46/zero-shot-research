de42ae8f5b6c70652180212cc8d455e4d52a3252
btt46 <tuanthanhbui46@gmail.com>
2022-04-12 15:50:49 +0900

    updatew

--- eval_ja2vi.sh
+++ eval_ja2vi.sh
@@ -11,11 +11,12 @@ tgt=$2
 GPUS=$3
 MODEL_NAME=$4
 MODEL=$PWD/models/${MODEL_NAME}/checkpoint_best.pt
-
+BIN_DATA=$EXPDIR/data/tmp/bin-data
+TAGGED_DATA=$EXPDIR/data/evaluation/tagged-data
 ########################## Validation dataset #########################################
 
 CUDA_VISIBLE_DEVICES=$GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA \
-            --input $BPE_DATA/valid.${src} \
+            --input $TAGGED_DATA/valid.${src} \
             --path $MODEL \
             --beam 5 | tee ${PWD}/results/${src}2${tgt}/valid_trans_result.${tgt}
 
@@ -30,3 +31,22 @@ python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${src}2${tgt}/valid_detr
 python3.6 $PWD/preprocess/normalize.py ${PWD}/results/${src}2${tgt}/valid.${tgt} ${PWD}/results/${src}2${tgt}/valid_normalize.${tgt}
 echo "VALID" >> ${PWD}/results/${src}2${tgt}/valid_result.txt
 perl $PWD/multi-bleu.pl $PWD/data/tmp/normalized/valid.${tgt} < ${PWD}/results/${src}2${tgt}/valid_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
+
+########################## Test dataset #########################################
+
+CUDA_VISIBLE_DEVICES=$GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA \
+            --input $TAGGED_DATA/test.${src} \
+            --path $MODEL \
+            --beam 5 | tee ${PWD}/results/${src}2${tgt}/test_trans_result.${tgt}
+
+grep ^H ${PWD}/results/${src}2${tgt}/test_trans_result.${tgt} | cut -f3 > ${PWD}/results/${src}2${tgt}/test_trans.${tgt}
+cat ${PWD}/results/${src}2${tgt}/test_trans.${tgt} | sed -r 's/(@@ )|(@@ ?$)//g' > ${PWD}/results/${src}2${tgt}/test_rmvbpe.${tgt}
+
+# detruecase
+$DETRUECASER < ${PWD}/results/${src}2${tgt}/test_rmvbpe.${tgt} > ${PWD}/results/${src}2${tgt}/test_detruecase.${tgt}
+
+# detokenize
+python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${src}2${tgt}/test_detruecase.${tgt} ${PWD}/results/${src}2${tgt}/test.${tgt}
+python3.6 $PWD/preprocess/normalize.py ${PWD}/results/${src}2${tgt}/test.${tgt} ${PWD}/results/${src}2${tgt}/test_normalize.${tgt}
+echo "test" >> ${PWD}/results/${src}2${tgt}/test_result.txt
+perl $PWD/multi-bleu.pl $PWD/data/tmp/normalized/test.${tgt} < ${PWD}/results/${src}2${tgt}/test_normalize.${tgt} >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
