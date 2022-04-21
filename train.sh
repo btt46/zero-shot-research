199aa060755c3b661d9af56e814c7dbbd834fe88
btt46 <tuanthanhbui46@gmail.com>
2022-04-22 01:52:27 +0900

    update

--- train.sh
+++ train.sh
@@ -9,10 +9,8 @@ DATASET=$PWD/data
 BIN_DATA=$DATASET/tmp/bin-data
 
 CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $BIN_DATA \
-                    # -s ${src} -t ${tgt} \
                     --task multilingual_translation --lang-pairs en-vi, vi-en, en-ja, ja-en \
                     --arch multilingual_transformer_iwslt_de_en \
-		            # --arch transformer_iwslt_de_en 
                     --share-decoder-input-output-embed \
                     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
                     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
